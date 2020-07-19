92b2f7f5b774de952a56f7c98cd051b898457fa4
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-19 21:30:11 +0200

    fixed  layout and class

--- src/com/stevnsvig/jenkins/release/Release.groovy
+++ src/com/stevnsvig/jenkins/release/Release.groovy
@@ -1,38 +1,43 @@
 #!groovy
 package com.stevnsvig.jenkins.release
+
 //Jenkins Pipeline release utilities
 
+/**
+  * Functions to ease Release process
+  */
 class Release {
- def get_branch_type(String branch_name) {
-     def dev_pattern = ".*development"
-     def release_pattern = ".*release/.*"
-     def feature_pattern = ".*feature/.*"
-     def hotfix_pattern = ".*hotfix/.*"
-     def master_pattern = ".*master"
-     if (branch_name =~ dev_pattern) {
-         return "dev"
-     } else if (branch_name =~ release_pattern) {
-         return "release"
-     } else if (branch_name =~ master_pattern) {
-         return "master"
-     } else if (branch_name =~ feature_pattern) {
-         return "feature"
-     } else if (branch_name =~ hotfix_pattern) {
-         return "hotfix"
-     } else {
-         return null;
-     }
- }
- 
- def get_branch_deployment_environment(String branch_type) {
-     if (branch_type == "dev") {
-         return "dev"
-     } else if (branch_type == "release") {
-         return "staging"
-     } else if (branch_type == "master") {
-         return "prod"
-     } else {
-         return null;
-     }
- }
+
+    def get_branch_type(String branchName) {
+        def dev_pattern = '.*development'
+        def release_pattern = '.*release/.*'
+        def feature_pattern = '.*feature/.*'
+        def hotfix_pattern = '.*hotfix/.*'
+        def master_pattern = '.*master'
+        if (branchName =~ dev_pattern) {
+            return 'dev'
+        } else if (branchName =~ release_pattern) {
+            return 'release'
+        } else if (branchName =~ master_pattern) {
+            return 'master'
+        } else if (branchName =~ feature_pattern) {
+            return 'feature'
+        } else if (branchName =~ hotfix_pattern) {
+            return 'hotfix'
+        } else {
+            return ''
+        }
+    }
+
+    def get_branch_deployment_environment(String branchType) {
+        if (branchType == 'dev') {
+            return 'dev'
+        } else if (branchType == 'release') {
+            return 'staging'
+        } else if (branchType == 'master') {
+            return 'prod'
+        }
+        return ''
+    }
+
 }
