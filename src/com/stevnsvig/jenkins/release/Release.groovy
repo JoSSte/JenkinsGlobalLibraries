a31eff23057361a04b0ac42185761a0ac39b983a
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-19 21:38:02 +0200

    code cleanup

--- src/com/stevnsvig/jenkins/release/Release.groovy
+++ src/com/stevnsvig/jenkins/release/Release.groovy
@@ -3,38 +3,44 @@ package com.stevnsvig.jenkins.release
 
 //Jenkins Pipeline release utilities
 
-/**
+/**@
   * Functions to ease Release process
   */
 class Release {
 
-    def get_branch_type(String branchName) {
-        def dev_pattern = '.*development'
-        def release_pattern = '.*release/.*'
-        def feature_pattern = '.*feature/.*'
-        def hotfix_pattern = '.*hotfix/.*'
-        def master_pattern = '.*master'
-        if (branchName =~ dev_pattern) {
-            return 'dev'
-        } else if (branchName =~ release_pattern) {
-            return 'release'
-        } else if (branchName =~ master_pattern) {
-            return 'master'
-        } else if (branchName =~ feature_pattern) {
-            return 'feature'
-        } else if (branchName =~ hotfix_pattern) {
-            return 'hotfix'
-        } else {
-            return ''
+    String devBranchName = 'dev'
+    String releaseBranchName = 'release'
+    String masterBranchName = 'master'
+    String hotfixBranchName = 'hotfix'
+    String featureBranchName = 'feature'
+
+
+    String  get_branch_type(String branchName) {
+        devPattern = '.*development'
+        releasePattern = '.*release/.*'
+        featurePattern = '.*feature/.*'
+        hotfixPattern = '.*hotfix/.*'
+        masterPattern = '.*master'
+        if (branchName =~ devPattern) {
+            return devBranchName
+        } else if (branchName =~ releasePattern) {
+            return relBranchName
+        } else if (branchName =~ masterPattern) {
+            return masterBranchName
+        } else if (branchName =~ featurePattern) {
+            return featureBranchName
+        } else if (branchName =~ hotfixPattern) {
+            return  hotfixBranchName
         }
+        return ''
     }
 
-    def get_branch_deployment_environment(String branchType) {
-        if (branchType == 'dev') {
-            return 'dev'
-        } else if (branchType == 'release') {
+    String  get_branch_deployment_environment(String branchType) {
+        if (branchType == devBranchName) {
+            return devBranchName
+        } else if (branchType == relBranchName) {
             return 'staging'
-        } else if (branchType == 'master') {
+        } else if (branchType == masterBranchName) {
             return 'prod'
         }
         return ''
