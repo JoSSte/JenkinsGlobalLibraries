33046e9529bbd3359d5b3133c35fc5018a21e1da
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-20 13:30:48 +0200

    added reference

--- test/ReleaseUtilTest.groovy
+++ test/ReleaseUtilTest.groovy
@@ -1,4 +1,6 @@
 // http://dzone.com/articles/writing-unit-tests-using-groov
+// https://groovy-lang.org/testing.html
+// https://stackoverflow.com/questions/27855989/running-groovy-tests-with-maven
 class ReleaseUtilTest extends GroovyTestCase {
 
     void testSomething(){
