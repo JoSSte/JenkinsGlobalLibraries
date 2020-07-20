eee452561b5adf1aaf5f1f938fb31587259ed50c
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-20 17:06:10 +0200

    working base tests

--- test/com/stevnsvig/jenkins/release/ReleaseUtilTest.groovy
+++ test/com/stevnsvig/jenkins/release/ReleaseUtilTest.groovy
@@ -1,3 +1,9 @@
+package com.stevnsvig.jenkins.release
+
+import org.junit.Test
+import junit.framework.TestCase
+import groovy.test.GroovyTestCase
+
 // http://dzone.com/articles/writing-unit-tests-using-groov
 // https://groovy-lang.org/testing.html
 // https://stackoverflow.com/questions/27855989/running-groovy-tests-with-maven
