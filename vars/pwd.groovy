df9d7f1f348e91061af99024bbc7e2f8191be318
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2020-07-20 21:58:21 +0200

    added hopefully the right import

--- vars/pwd.groovy
+++ vars/pwd.groovy
@@ -1,3 +1,6 @@
+import org.jenkinsci.plugins.workflow.steps.durable_task.ShellStep
+
+
 static String getPWD() {
         def ret = sh(returnStdout: true, script: "git tag --sort version:refname | tail -1").trim()
         eho "currently in ${ret}"
