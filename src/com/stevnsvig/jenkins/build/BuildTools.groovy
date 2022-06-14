53300a0cecb7576966a8ae08c4f2ad6e5607f448
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2022-06-14 16:22:22 +0200

    add BuilTools class

--- src/com/stevnsvig/jenkins/build/BuildTools.groovy
+++ src/com/stevnsvig/jenkins/build/BuildTools.groovy
@@ -1,13 +1,26 @@
-def changeLogSets = currentBuild.changeSets
-for (int i = 0; i < changeLogSets.size(); i++) {
-    def entries = changeLogSets[i].items
-    for (int j = 0; j < entries.length; j++) {
-        def entry = entries[j]
-        echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-        def files = new ArrayList(entry.affectedFiles)
-        for (int k = 0; k < files.size(); k++) {
-            def file = files[k]
-            echo "  ${file.editType.name} ${file.path}"
+#!groovy
+package com.stevnsvig.jenkins.build
+
+//Jenkins Pipeline build utilities
+
+/**@
+  * Functions to ease build process
+  */
+class BuildTools {
+    static String getModifiedConfig() {
+
+        def changeLogSets = currentBuild.changeSets
+        for (int i = 0; i < changeLogSets.size(); i++) {
+            def entries = changeLogSets[i].items
+            for (int j = 0; j < entries.length; j++) {
+                def entry = entries[j]
+                echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+                def files = new ArrayList(entry.affectedFiles)
+                for (int k = 0; k < files.size(); k++) {
+                    def file = files[k]
+                    echo "  ${file.editType.name} ${file.path}"
+                }
+            }
         }
     }
 }
